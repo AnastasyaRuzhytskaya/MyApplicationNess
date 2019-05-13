411cdd4b5e54b805869ad329a37b8e475a7fcaa9
NESS <ness@gmail.com>
2019-05-13 13:16:05 +0300

    Instead of time, the date is now displayed.

--- app/src/main/java/ua/kiev/netmaster/razer/myapplication/Violet.java
+++ app/src/main/java/ua/kiev/netmaster/razer/myapplication/Violet.java
@@ -3,21 +3,44 @@ package ua.kiev.netmaster.razer.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by RAZER on 08-Feb-18.
- */
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
-public class Violet extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+public class Violet extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.violet);
-
-
-
-
-
-
-
-
+        Log.d(LOG_TAG, "MainActivity onCreate");
     }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "MainActivity onStart");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "MainActivity onResume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "MainActivity onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "MainActivity onStop");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "MainActivity onDestroy");
+    }
+
 }
+

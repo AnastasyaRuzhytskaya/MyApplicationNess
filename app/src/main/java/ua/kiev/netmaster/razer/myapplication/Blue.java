3c026a0bdaeb76b70c88669b0d94fb59e9641fb0
gotik <gotikany@gmail.com>
2019-06-04 20:25:24 +0300

    Add fragment om button "seek bar"

--- app/src/main/java/ua/kiev/netmaster/razer/myapplication/Blue.java
+++ app/src/main/java/ua/kiev/netmaster/razer/myapplication/Blue.java
@@ -1,5 +1,6 @@
 package ua.kiev.netmaster.razer.myapplication;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,7 +9,8 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class Blue extends AppCompatActivity implements View.OnClickListener {
+
+public class Blue extends Fragment {
 
     Button btnWeb;
     Button btnMap;
